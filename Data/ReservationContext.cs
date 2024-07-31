267730b68e0e9bef214eead56a84c83aa65906ab
MatheusGomes <matheus2111gomes@gmail.com>
2024-07-31 14:59:32 -0300

    first commit/config entity

---
+++ Data/ReservationContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace ReservationSystem.Datapace
+{
+    public class ReservationContext : DbContext
+    {
+        public ReservationContext(DbContextOptions<ReservationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Availability> Availability { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurações adicionais para as entidades
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<UserRole>()
+                .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+
+            // Outras configurações de relacionamento, chaves e restrições
+        }
+    }
+}
