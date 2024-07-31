1a6e6f09031d6d17dcd643187dcde182f8c50dda
MatheusGomes <matheus2111gomes@gmail.com>
2024-07-31 15:11:49 -0300

    adding user,role,userRole models

--- Data/ReservationContext.cs
+++ Data/ReservationContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Models;
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection.Emit;
@@ -15,17 +16,12 @@ namespace ReservationSystem.Datapace
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
-        public DbSet<Resource> Resources { get; set; }
-        public DbSet<Reservation> Reservations { get; set; }
-        public DbSet<Availability> Availability { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-        public DbSet<Comment> Comments { get; set; }
+   
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Configurações adicionais para as entidades
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Email)
                 .IsUnique();
@@ -43,7 +39,6 @@ namespace ReservationSystem.Datapace
                 .WithMany(r => r.UserRoles)
                 .HasForeignKey(ur => ur.RoleId);
 
-            // Outras configurações de relacionamento, chaves e restrições
         }
     }
 }
